8fc288fcef94ff56181dbc41d5f8e3827c79da4d
Emerson Jacobson <emwjacobson@gmail.com>
2020-09-16 23:59:33 +0000

    Added about page

--- app/views.py
+++ app/views.py
@@ -4,6 +4,19 @@ from django.http import HttpResponse
 projects = [
     {
         'image': 'https://i.imgur.com/THug83T.png',
+        'title': 'this',
+        'description': 'With a bit of downtime between my Summer Research Program and my first Quarter at UC Riverside, I decided it was time to make myself a basic little portfolio website where I can show off some of the work that I am proud to have made.',
+        'technologies': [
+            "Docker & Docker-Compose",
+            "Django"
+        ],
+        'links': [
+            { 'text': 'Demo', 'url': 'https://emwj.dev/' },
+            { 'text': 'GitHub (TODO)', 'url': 'https://github.com/emwjacobson/'}
+        ]
+    },
+    {
+        'image': 'https://i.imgur.com/THug83T.png',
         'title': 'MCHost',
         'description': 'This project aims to create on-demand Minecraft Servers. I used Docker in order to streamline development and make the creation/destruction of servers extremely easy. I used Django as the Frontend as I wanted to learn more about it.',
         'technologies': [
@@ -15,7 +28,22 @@ projects = [
             { 'text': 'GitHub', 'url': 'https://github.com/emwjacobson/MCHoster'}
         ]
     },
+    {
+        'image': 'https://i.imgur.com/K6mEpUM.png',
+        'title': 'Pypc2',
+        'description': 'For a while I was really into Project Cars 2, so I created a custom dashboard to be displayed while driving. It includes the car\'s current RPM, speed, and gear, as well as a meter showing when to shift, current throttle/braking, and g-forces applied to the car.',
+        'technologies': [
+            "Python",
+            "Pygame"
+        ],
+        'links': [
+            { 'text': 'GitHub', 'url': 'https://github.com/emwjacobson/pypc2'}
+        ]
+    },
 ]
 
 def index(request):
     return render(request, 'app/index.html', {'projects': projects})
+
+def about(request):
+    return render(request, 'app/about.html')
