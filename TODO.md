93570d9783dbc514ee2671aec27a76a903b24281
Emerson Jacobson <emwjacobson@gmail.com>
2021-01-28 18:46:52 +0000

    Updated website

--- TODO.md
+++
@@ -1,6 +0,0 @@
-# TODO List
-
-- Make SECRET_KEY an environmental variable
-- Static files for images
-- Use database for entries?
-    - Ill only have a couple, might be overcomplicating things
--- web/app/views.py
+++ web/app/views.py
@@ -5,7 +5,7 @@ projects = [
     {
         'image': 'https://i.imgur.com/lY40B7X.png',
         'title': 'This Website!',
-        'description': 'With a bit of downtime between my Summer Research Program and my first Quarter at UC Riverside, I decided it was time to make myself a basic little portfolio website where I can show off some of the work that I am proud to have made.',
+        'description': 'With a bit of downtime between my Summer Research Program and my first Quarter at the University of California, Riverside, I decided it was time to make myself a portfolio website. Here I can show off some of the work that I am proud to have made.',
         'technologies': [
             "Docker & Docker-Compose",
             "Django"
@@ -17,10 +17,10 @@ projects = [
     },
     {
         'image': 'https://i.imgur.com/THug83T.png',
-        'title': 'MCHost',
-        'description': 'This project aims to create on-demand Minecraft Servers. I used Docker in order to streamline development and make the creation/destruction of servers extremely easy. I used Django as the Frontend as I wanted to learn more about it.',
+        'title': 'MCHoster',
+        'description': 'This project aims to create on-demand Minecraft Servers. I used Docker to streamline development and make the creation/destruction of servers extremely easy. I used Django as the Frontend as I wanted to learn more about it. I later expanded on this project to use Docker Swarm, allowing for High Availability and distribution of the load across multiple servers.',
         'technologies': [
-            "Docker & Docker-Compose",
+            "Docker, Compose, Swarm",
             "Django"
         ],
         'links': [
@@ -31,7 +31,7 @@ projects = [
     {
         'image': 'https://i.imgur.com/K6mEpUM.png',
         'title': 'Pypc2',
-        'description': 'For a while I was really into Project Cars 2, so I created a custom dashboard to be displayed while driving. It uses data from the game to display the car\'s current RPM, speed, and gear, as well as a meter showing when to shift, current throttle/braking, and g-forces applied to the car.',
+        'description': 'For a while, I was really into Project Cars 2, so I created a custom dashboard to be displayed while driving. It uses data from the game to display the car\'s current RPM, speed, and gear. It also has a meter showing when to shift, current throttle/braking, and g-forces applied to the car.',
         'technologies': [
             "Python",
             "Pygame"
