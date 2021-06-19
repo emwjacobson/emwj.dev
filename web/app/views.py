98310e2aa6b1967cd521d4fa50412e9c7f435f7a
Emerson Jacobson <emwjacobson@gmail.com>
2021-06-19 04:55:59 +0000

    Moved images to own site rather than imgur

--- web/app/views.py
+++ web/app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.templatetags.static import static
 
 projects = [
     {
-        'image': 'https://i.imgur.com/lY40B7X.png',
+        'image': static('site.png'),
         'title': 'This Website!',
         'description': 'With a bit of downtime between my Summer Research Program and my first Quarter at the University of California, Riverside, I decided it was time to make myself a portfolio website. Here I can show off some of the work that I am proud to have made.',
         'technologies': [
@@ -16,7 +16,7 @@ projects = [
         ]
     },
     {
-        'image': 'https://i.imgur.com/MC4wYrG.png',
+        'image': static('r510.png'),
         'title': 'Self-hosted Homelab',
         'description': 'I run a Dell R510 with dual 6C/12T Xeon X5650 processors and 112 GB of Memory. I have Proxmox installed and use it to virtualize many servers. Some services I run are: PiHole for DNS Adblocking, an NFS for file storage, Plex for home media streaming, a machine for running Docker images, NextCloud, and Nginx as a reverse proxy.',
         'technologies': [
@@ -29,7 +29,7 @@ projects = [
         'links': []
     },
     {
-        'image': 'https://i.imgur.com/THug83T.png',
+        'image': static('mchost.png'),
         'title': 'MCHoster',
         'description': 'This project aims to create on-demand Minecraft Servers. I used Docker to streamline development and make the creation/destruction of servers extremely easy. I used Django as the Frontend as I wanted to learn more about it. I later expanded on this project to use Docker Swarm, allowing for High Availability and distribution of the load across multiple servers.',
         'technologies': [
@@ -42,7 +42,7 @@ projects = [
         ]
     },
     {
-        'image': 'https://i.imgur.com/K6mEpUM.png',
+        'image': static('pc2.png'),
         'title': 'Pypc2',
         'description': 'For a while, I was really into Project Cars 2, so I created a custom dashboard to be displayed while driving. It uses data from the game to display the car\'s current RPM, speed, and gear. It also has a meter showing when to shift, current throttle/braking, and g-forces applied to the car.',
         'technologies': [
